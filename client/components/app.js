263904da503f675a4eff5d780789912e093997ec
Scott Mitchell <smitche60@gmail.com>
2017-12-11 22:43:42 -0800

    Add click handler

--- client/components/app.js
+++ client/components/app.js
@@ -6,7 +6,20 @@ app.component('app', {
 });
 
 app.controller('appController', ($scope, $http) => {
-  $scope.board = [[1, 1, 1, 1], [0, 1, 0, 0], [1, 0, 1, 1]];
+  $scope.board = null;
+
+  $scope.handleclick = (number) => {
+    console.log("clicked")
+    console.log(number);
+    $scope.getStartingBoard(number, number);
+  };
+
+  $scope.getStartingBoard = (x, y) => {
+    $http.post('/start', { x, y })
+      .then((response) => {
+        $scope.board = response.data;
+      });
+  };
 
   $scope.getSuccessor = (currentBoard) => {
     $http.post('/successor', { currentBoard })
@@ -19,49 +32,3 @@ app.controller('appController', ($scope, $http) => {
     $scope.getSuccessor($scope.board);
   }, 1000);
 });
-
-// app.controller('appController', ($scope, $http) => {
-//
-//   $scope.board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-//
-//   $scope.getSuccessor = (currentBoard) => {
-//     $http.post('/successor', { currentBoard: currentBoard })
-//     .then(function(response) {
-//       $scope.board = response.data;
-//     }.bind(this));
-//   };
-//
-//   setInterval(() => {
-//     $scope.getSuccessor($scope.board);
-//   }, 1000);
-//
-//
-//
-// });
-
-
-
-// $scope.getSuccessor($scope.board);
-
-// $http.post('/successor', { currentBoard: [[1, 1, 1], [1, 1, 1], [1, 1, 1]] })
-// .then(function(response) {
-//   $scope.board = response.data;
-// }.bind(this));
-
-//
-// app.service('generateSuccessor', () => {
-//   this.ones = () => [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-// });
-
-//const app = angular.module('app', []);
-
-// app.controller('appController', ['generateSuccessor', '$scope', ($scope, generateSuccessor) => {
-//   $scope.callgenerateSuccessor = () => {
-//     generateSuccessor();
-//   };
-// }]);
-//
-// app.factory('generateSuccessor', [() => {
-//   console.log([[1, 1, 1], [1, 1, 1], [1, 1, 1]]);
-//   return [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-// }]);
