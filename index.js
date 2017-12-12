781a3fd7f06ae894175355baa591da5b4189e08c
Scott Mitchell <smitche60@gmail.com>
2017-12-11 16:43:50 -0800

    Add server and endpoint

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logic = require('./server/logic.js');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Render static assets
+app.use(express.static(__dirname + '/client'));
+
+// Listen at the specified port
+const port = 8000;
+app.listen(port, () => {
+  console.log('Listening on port :', port);
+});
+
+app.post('/successor', (req, res) => {
+  const successor = logic.generateSuccessorBoard(req.body.currentBoard);
+  res.send(successor);
+});
+
+module.exports = app;
